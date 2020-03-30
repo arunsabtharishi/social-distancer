0832f4be7d8a6289aca41dcd9e091968b4fb883d
Arun Sabtharishi <arun.sabtharishi@gmail.com>
2020-03-30 01:54:44 -0500

    Create apis.js

---
+++ app/utils/apis.js
@@ -0,0 +1,14 @@
+const pingapi = `http://localhost:8071/update-device-location`
+
+
+
+export function pingDeviceLocation (obj) {
+  const headers = new Headers();
+  headers.append('Content-Type', 'application/json');
+  return fetch(pingapi, {
+    method: 'post',
+    headers: headers,
+    body: JSON.stringify(obj)
+  })
+}
+
